0128f5189e201a0c4f939d26be8ee02c1e64cad3
grhav <grhav@DESKTOP-1335TBB>
2021-04-04 17:13:06 +1000

    Prototype 1

---
+++ Prototype_1/src/test/java/maventestngint/Demo.java
@@ -0,0 +1,13 @@
+package maventestngint;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void testA () {
+		Reporter.log("Maven TESTNG Integration");
+	}
+
+}
